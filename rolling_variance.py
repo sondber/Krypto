Fixed rolling variance and changed variable names

rolling_variance.py
@@ -3,14 +3,17 @@ import csv
 import jacob_csv_handling
 import matplotlib.pyplot as plt
 
-def varians(returns,units_rolling): #takes in the list of logreturns and minutes rolling required
-    var = np.zeros(len(returns)/units_rolling)#to be returned
+def variance(returns,units_rolling): # takes in the list of logreturns and minutes rolling required
+    var = np.zeros(int(len(returns)/units_rolling)) # to be returned
+    j = 0
+    hourlist = np.zeros(int(len(returns)/units_rolling))
     for i in range(0, len(returns)-units_rolling+1, units_rolling):
-        j = units_rolling+i-1
-        var[j] = np.var(returns[i:(i+units_rolling)]) # are we confident on this var-calculation, ref is set observed or real?
+        var[j] = np.var(returns[i:(i+units_rolling)]) # are we confident on this var-calculation
+        hourlist[j] = int(j)
+        j += 1
         if i%100000==0:
             print("Progress in calculation:", round(i/(len(returns)-units_rolling+1)*100, 2), "%")
-    return var
+    return var, hourlist
 
 
 file_name =  "data/export_csv/logreturns_all_minute.csv"
@@ -27,10 +30,10 @@ with open(file_name, newline='') as csvfile:
                 i = i + 1
 print("The data is loaded")
 
-variance = varians(returns, 60) #one hour variances
+variance, hourlist = variance(returns, 60) #one hour variances
 
 plt.plot(variance)
 plt.show()
 
 print("The variances are calculated")
-jacob_csv_handling.write_to_file(time_list, variance, "data/export_csv/rolling_variance_all.csv","Variance")
+jacob_csv_handling.write_to_file(hourlist, variance, "data/export_csv/rolling_variance_all.csv", "One-hour variance")
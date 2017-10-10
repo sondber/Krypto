224955be63b9cc59889c6ee268a48f419742c990
jetonnesen <jetonnesen@users.noreply.github.com>
2017-10-10 12:59:46 +0200

    update1259

--- rolling_variance.py
+++ rolling_variance.py
@@ -1,18 +1,19 @@
 import numpy as np
 import csv
 import jacob_csv_handling
+import matplotlib.pyplot as plt
 
-def varians(returns,minutes_rolling): #takes in the list of logreturns and days rolling required
-    var = np.zeros(len(returns)) #to be returned
-    for i in range(len(returns)-minutes_rolling+1):
-        j = days_rolling+i-1
-        var[j] = np.var(returns[i:(i+minutes_rolling)])
+def varians(returns,units_rolling): #takes in the list of logreturns and minutes rolling required
+    var = np.zeros(len(returns)/units_rolling)#to be returned
+    for i in range(0, len(returns)-units_rolling+1, units_rolling):
+        j = units_rolling+i-1
+        var[j] = np.var(returns[i:(i+units_rolling)]) # are we confident on this var-calculation, ref is set observed or real?
         if i%100000==0:
-            print("Progress in calculation:", round(i/(len(returns)-minutes_rolling+1)*100, 2), "%")
+            print("Progress in calculation:", round(i/(len(returns)-units_rolling+1)*100, 2), "%")
     return var
 
 
-file_name =  "data/export_csv/logreturns_all_minnute.csv"
+file_name =  "data/export_csv/logreturns_all_minute.csv"
 returns = []
 time_list = []
 
@@ -27,6 +28,9 @@ with open(file_name, newline='') as csvfile:
 print("The data is loaded")
 
 variance = varians(returns, 60) #one hour variances
+
+plt.plot(variance)
+plt.show()
 
 print("The variances are calculated")
 jacob_csv_handling.write_to_file(time_list, variance, "data/export_csv/rolling_variance_all.csv","Variance")

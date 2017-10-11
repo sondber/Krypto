e2f62324c98d1bbeeffb05240d7eade79fe8e080
sondber <sondrehb@stud.ntnu.no>
2017-10-11 09:36:16 +0200

    Try in logreturn, (jacob_support)

--- sondre_test_script.py
+++ sondre_test_script.py
@@ -1,4 +1,10 @@
 import data_import as di
-
+import plot
+import sondre_support_formulas as supp
+import jacob_support as jake_supp
 
 exchanges, time_list, prices, volumes, total_price, total_volume, currency = di.get_lists(2, 1)
+print(total_price[0:100])
+returns = jake_supp.logreturn(total_price[0:100])
+volume = total_volume[0:100]
+plot.scatters(volume, returns)

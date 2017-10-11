adde5c943e36ae2d864725dd930e7adaef6add2f
sondber <sondrehb@stud.ntnu.no>
2017-10-11 09:14:56 +0200

    Scatterplot

--- plot.py
+++ plot.py
@@ -178,3 +178,16 @@ def easy_plot(y, label, show_plot):
         plt.plot(y)
     if show_plot == 1:
         plt.show()
+
+
+def scatters(x, y, groups=[], areas=[]):
+    n = len(x)
+    if groups:
+        colors = groups
+    else:
+        colors = np.random.rand(n)
+    if not areas:
+        areas = 1 * np.ones(n)
+
+    plt.scatter(x, y, s=areas, c=colors, alpha=0.5)
+    plt.show()

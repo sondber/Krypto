4667e945e1eb322b1bd27cc00bbd516f9fd4c51d
jetonnesen <jetonnesen@users.noreply.github.com>
2017-10-25 12:18:06 +0200

    update

--- rolls_new.py
+++ rolls_new.py
@@ -1,77 +1,118 @@
 import math
 import data_import as di
 from Jacob import jacob_csv_handling, jacob_support
+from Sondre import sondre_support_formulas as supp
 import matplotlib.pyplot as plt
+#import two_axis_plot as two_plot
 
 #The following estimation of Rolls estimator is based on the formula in Haugom, Molnar (2014)
 
 
-def rolls_new(price_differences, time_list_minute, window):
-    calculation_inside = 0
-    spread = [0]
+def rolls(prices, price_differences, time_list_minute, window):
+    spread = []
+    spread_rel = []
     time_list_hour = []
-    time_list_hour.append(time_list_minute[0])
     count_value_error = 0
-    for i in range(0, len(price_differences)-window, window):
-        # print("Outer loop: ", i)
-        time_list_hour.append(time_list_minute[i])
-        for y in range(i+1,i+window+1):
-            # print("Inner loop: ",y)
-            calculation_inside = calculation_inside + (price_differences[y]*price_differences[y-1])
-        try:
-            ba_calc = 2*math.sqrt(-calculation_inside/(window-1))
-        except ValueError:
-            count_value_error += 1
-            ba_calc = 0
-        spread.append(ba_calc)
-    return spread, time_list_hour, count_value_error
+    half_hour = 30
+    pos = 0 # position in price diff vector
+    half = 1 # indicates that one half hour must be accounted for
+    tod = 0 # tod to keep track of when to reset half
+    sum_inside = 0
+    while pos < len(price_differences)-window:
+        if half == 1:
+            for i in range(pos+1,pos+half_hour+1):
+                sum_inside = sum_inside + (price_differences[i]*price_differences[i-1])
+            try:
+                ba_calc = 2*math.sqrt(-sum_inside/(half_hour-1))
+            except ValueError:
+                count_value_error += 1
+                ba_calc = 0
+            spread.append(ba_calc)
+            time_list_hour.append(time_list_minute[pos])
+            spread_rel.append(ba_calc/prices[pos])
+            half = 0
+            pos += 30
+            sum_inside = 0
+        else:
+            for i in range(pos+1, pos+window+1):
+                sum_inside = sum_inside + (price_differences[i]*price_differences[i-1])
+            try:
+                ba_calc = 2*math.sqrt(-sum_inside/(window-1))
+            except ValueError:
+                count_value_error += 1
+                ba_calc = 0
+            spread.append(ba_calc)
+            time_list_hour.append(time_list_minute[pos])
+            spread_rel.append(ba_calc/prices[pos])
+            pos += 60
+            sum_inside = 0
+            if tod == 6:
+                tod == 0
+                half = 1
+            else:
+                tod += 1
+    return spread, spread_rel, time_list_hour, count_value_error
 
 
-load_price_differences = int(input("Do you want to calculate price differences?"))
+load_price_differences = 1
 if load_price_differences:
-    file_name = input("From what file do you want to load data? (raw data with time, price and volume) ")
-    to_file = input("Name the output file: ") # should be named price differences something
+    # file_name = input("From what file do you want to load data? (raw data with time, price and volume) ")
+    to_file = "price_diffs_test.csv"
     time_list = []
     prices = []
     volume = []
-    data = jacob_csv_handling.read_single_csv(file_name, time_list, prices, volume)
-    # data = di.get_lists(data="p")
-    price_differences = jacob_support.first_price_differences(data[1]) # calculates price differences
-    time_list = data[0]
-    jacob_csv_handling.write_to_file(data[0], price_differences, to_file, "Price_differences")
-else:
-    file_name = input("From what file do you want to load price differences? ")
-    time_list = []
-    price_differences = []
-    volume = []
-    data = jacob_csv_handling.read_single_csv(file_name, time_list, price_differences) #price diffs now stored in data[1]
-    price_differences = data[1]
-    time_list = data[0]
+    time_list = di.get_lists()[1]
+    total_price = di.get_lists()[4]
+    # data = jacob_csv_handling.read_single_csv(file_name, time_list, total_price, total_volume)
+    price_differences = jacob_support.first_price_differences(total_price) # calculates price difference
+    jacob_csv_handling.write_to_file(time_list, price_differences, to_file, "Price_differences")
 
 window1 = 60 # calculate spread for this period of minutes
-window2 = 60*8
+# window2 = 60*8
 
 print("The length of the price_diff: ", len(price_differences))
 
 print("Calculates BA-spread:")
 
-spread1, time_list_hour1, count_value_error_1 = rolls_new(price_differences, time_list, window1)
-spread2, time_list_hour2, count_value_error_2 = rolls_new(price_differences, time_list, window2)
+spread1, spread1_rel, time_list_hour1, count_value_error_1 = rolls(total_price, price_differences, time_list, window1)
+# spread2, time_list_hour2, count_value_error_2 = rolls_new(price_differences, time_list, window2)
+
+
+
+"""
+for i in range(0,len(spread2)):
+    spread2_rel.append(spread2[i]/total_price[i*window2])
+"""
+
 
 print(len(spread1))
-print(len(spread2))
+print(len(spread1_rel))
 
-print("The following is the BAs with window",window1)
+# print(len(spread2))
+
+print("The following is the BAs with window", window1)
 print(time_list_hour1)
 print(spread1)
-print("With",window1,"window,",count_value_error_1,"value errors were counted")
+print(spread1_rel)
+print("With", window1, "window,", count_value_error_1, "value errors were counted")
 
+"""
 print("The following is the BAs with window",window2)
 print(time_list_hour2)
 print(spread2)
 print("With",window2,"window,",count_value_error_2,"value errors were counted")
+"""
 
-plt.plot(spread1)
+x = []
+for i in range(0, len(spread1_rel)):
+    x.append(i)
+
+
+print(max(spread1_rel))
+
+
+plt.bar(x, spread1_rel)
+plt.ylim(0.0, 0.1)
 plt.show()
 
 

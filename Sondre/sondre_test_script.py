45f4c4c05aa91ac82ab9d3a01f52b0817ac39a88
sondber <sondrehb@stud.ntnu.no>
2017-10-30 17:23:23 +0100

    Cyclicalibæsj

--- Sondre/sondre_test_script.py
+++ Sondre/sondre_test_script.py
@@ -11,13 +11,3 @@ from matplotlib import pyplot as plt
 os.chdir("/Users/sondre/Documents/GitHub/krypto")
 
 exchanges, time_list, prices, volumes = di.get_lists(opening_hours="n", make_totals="n")
-plt.plot(prices[0, :])
-plt.ylim([0, 1000])
-plt.title("All hours")
-
-plt.figure(2)
-plt.title("Opening hours only")
-exchanges, time_list, prices, volumes = di.get_lists(opening_hours="y", make_totals="n")
-plt.plot(prices[0, :])
-plt.ylim([0, 1000])
-plt.show()

a0358e2e53c46b1d9e8db11b36760253a3fd20f2
jetonnesen <jetonnesen@users.noreply.github.com>
2018-03-05 15:57:12 +0100

    updated rolls

---
+++ Jacob/rolls_test.py
@@ -0,0 +1,9 @@
+from Sondre import sondre_test_script as testscript
+
+import os
+
+os.chdir("/Users/Jacob/Documents/GitHub/krypto/")
+
+
+testscript.
+
--- rolls.py
+++ rolls.py
@@ -3,6 +3,7 @@ from Sondre import sondre_support_formulas as supp
 import numpy as np
 import linreg
 
+
 # SISTE NYE FRA JACOB 16/11
 # The following estimation of Rolls estimator is based on the formula in Haugom, Molnar (2014)
 
@@ -17,7 +18,7 @@ def first_price_differences(prices):  # takes list of prices, returns equal leng
     return returnlist
 
 
-def rolls(prices_minute, time_list_minute, calc_basis=0, kill_output=0):  # calc_basis 0/1/2 hour/day/week
+def rolls(prices_minute, time_list_minute, calc_basis=0, kill_output=0):  # calc_basis 0/1 hour/day
     year, month, day, hour, minute = supp.fix_time_list(time_list_minute)
 
     spread = []
@@ -34,6 +35,7 @@ def rolls(prices_minute, time_list_minute, calc_basis=0, kill_output=0):  # calc
     if kill_output == 0:
         print("Price differences-calculation finished. ")
 
+    """
     # determine opening hours yes/no
     if hour[0] == 14:  # this indicates that opening times are being investigated
         opening_times = 1
@@ -41,117 +43,77 @@ def rolls(prices_minute, time_list_minute, calc_basis=0, kill_output=0):  # calc
     else:
         opening_times = 0
         opening_hours_desc = "full day"
+    """
 
-    # determine minutes_in_window based on opening_hours and calc_basis
-    if opening_times == 1:
-        if calc_basis == 1:  # daily with opening hours only
-            minutes_in_window = (6 * 60) + 30
-            freq_desc = "daily"
-        elif calc_basis == 2:  # weekly with openings hours only
-            minutes_in_window = ((6 * 60) + 30) * 5
-            freq_desc = "weekly"
-        else:  # hourly with openings hours. Minutes_in_window is not used in this case
-            freq_desc = "hourly"
-    else:
-        if calc_basis == 0:  # hourly with full data
-            minutes_in_window = 60
-            freq_desc = "hourly"
-        elif calc_basis == 1:  # daily with full data
-            minutes_in_window = 60 * 24
-            freq_desc = "daily"
-        else:  # weekly with full data
-            minutes_in_window = 60 * 24 * 7
-            freq_desc = "weekly"
+    # determine minutes_in_window based on calc_basis
+    if calc_basis == 0:  # hourly with full data
+        minutes_in_window = 60
+        freq_desc = "hourly"
+    else:  # daily with full data
+        minutes_in_window = 60 * 24
+        freq_desc = "daily"
 
     if kill_output == 0:
-        print("Calculating spreads on a/an", freq_desc, "basis using", opening_hours_desc, "data")
+        print("Calculating spreads on a/an", freq_desc, "basis")
 
-    if calc_basis == 0 and opening_times:  # opening times and hourly basis needs to account for half hours
-        half_hour = 30
-        window = 60
-        pos = 0  # position in price diff vector
-        half = 1  # indicates that one half hour must be accounted for
-        tod = 0  # tod to keep track of when to reset half
-        sum_inside = 0
-        while pos < len(price_differences):
-            if half == 1:
-                list1 = price_differences[pos:pos + half_hour - 1]
-                list2 = price_differences[pos + 1:pos + half_hour]
-                corr = np.corrcoef(list1, list2)[0, 1]
-                p_val = linreg.linreg_coeffs(list1, list2)[3]
-                if abs(corr) < corr_threshold:
-                    count_corr_below += 1
+    # calculation
 
-                for i in range(pos + 1, pos + half_hour):
-                    sum_inside = sum_inside + (price_differences[i] * price_differences[i - 1])
-                try:
-                    ba_calc = 2 * math.sqrt(-sum_inside / (half_hour - 2))
-                except ValueError:
-                    count_value_error += 1
-                    ba_calc = 0
-                    if p_val < alpha:
-                        alpha_count_below += 1
-                spread.append(ba_calc)
-                time_list.append(time_list_minute[pos])
-                if prices_minute[pos] == 0:
-                    print("Here is a zero!")
-                spread_rel.append(ba_calc / prices_minute[pos+half_hour-1])
-                half = 0
-                pos += 30
-                sum_inside = 0
-            else:
-                list1 = price_differences[pos:pos + window - 1]
-                list2 = price_differences[pos + 1:pos + window]
-                corr = np.corrcoef(list1, list2)[0, 1]
-                p_val = linreg.linreg_coeffs(list1, list2)[3]
-                if abs(corr) < corr_threshold:
-                    count_corr_below += 1
+    if calc_basis == 1:
+        if hour[0] == 0:
+            start_index = 0
+        else:
+            start_index = 24*60 - hour[0]*60  # this is for the continued calc after day 1
 
-
-                for i in range(pos + 1, pos + window):
-                    sum_inside = sum_inside + (price_differences[i] * price_differences[i - 1])
-                try:
-                    ba_calc = 2 * math.sqrt(-sum_inside / (window - 2))
-                except ValueError:
-                    count_value_error += 1
-                    ba_calc = 0
-                    if p_val < alpha:
-                        alpha_count_below += 1
-                spread.append(ba_calc)
-                time_list.append(time_list_minute[pos])
-                spread_rel.append(ba_calc / prices_minute[pos+window-1])
-                sum_inside = 0
-                if tod == 5:
-                    tod = 0
-                    half = 1
-                    pos += 60
-                else:
-                    tod += 1
-                    pos += 60
-    else:
-        sum_inside = 0
-        for i in range(0, len(price_differences) - minutes_in_window if calc_basis == 2 else len(price_differences),
-                       minutes_in_window):  # check this loop when weeks are monday-friday - at this point, the loop ends at the next last week
-            list1 = price_differences[i:i + minutes_in_window - 1]
-            list2 = price_differences[i + 1:i + minutes_in_window]
+            minutes_in_window_day1 = (24 - hour[0]) * 60
+            sum_inside = 0
+            list1 = price_differences[i:i + minutes_in_window_day1 - 1]
+            list2 = price_differences[i + 1:i + minutes_in_window_day1]
             corr = np.corrcoef(list1, list2)[0, 1]
             p_val = linreg.linreg_coeffs(list1, list2)[3]
             if abs(corr) < corr_threshold:
                 count_corr_below += 1
 
-            for y in range(i + 1, i + minutes_in_window):
+            for y in range(1, minutes_in_window_day1):
                 sum_inside = sum_inside + (price_differences[y] * price_differences[y - 1])
             try:
-                ba_calc = 2 * math.sqrt(-sum_inside / (minutes_in_window - 2))
+                ba_calc = 2 * math.sqrt(-sum_inside / (minutes_in_window_day1 - 2))
             except ValueError:
                 count_value_error += 1
                 ba_calc = 0
                 if p_val < alpha:
                     alpha_count_below += 1
             spread.append(ba_calc)
-            time_list.append(time_list_minute[i])
-            spread_rel.append(ba_calc / prices_minute[i+minutes_in_window-1])
-            sum_inside = 0
+            time_list.append(time_list_minute[0])
+            spread_rel.append(ba_calc / prices_minute[minutes_in_window_day1 - 1])
+    else:
+        start_index = 0
+
+    sum_inside = 0
+    for i in range(start_index, len(price_differences),
+                   minutes_in_window):
+        list1 = price_differences[i:i + minutes_in_window - 1]
+        list2 = price_differences[i + 1:i + minutes_in_window]
+        corr = np.corrcoef(list1, list2)[0, 1]
+        p_val = linreg.linreg_coeffs(list1, list2)[3]
+        if abs(corr) < corr_threshold:
+            count_corr_below += 1
+
+        for y in range(i + 1, min(i + minutes_in_window, len(time_list_minute))):
+            sum_inside = sum_inside + (price_differences[y] * price_differences[y - 1])
+        try:
+            ba_calc = 2 * math.sqrt(-sum_inside / (minutes_in_window - 2))
+        except ValueError:
+            count_value_error += 1
+            ba_calc = 0
+            if p_val < alpha:
+                alpha_count_below += 1
+        spread.append(ba_calc)
+        time_list.append(time_list_minute[i])
+        spread_rel.append(ba_calc / prices_minute[i + minutes_in_window - 1])
+        sum_inside = 0
+
+    # results
+
     if kill_output == 0:
         print("Spreads-calculation is finished")
         print("The length of the spread-vector is", len(spread_rel))
@@ -160,7 +122,8 @@ def rolls(prices_minute, time_list_minute, calc_basis=0, kill_output=0):  # calc
               "value errors were counted when calculating Roll-spreads")
         print(count_corr_below, "correlations below threshold(", corr_threshold, ") were counted(",
               round(100 * (count_corr_below / len(spread_rel)), 2), "%)")
-        print(alpha_count_below, "of the  value-error correlations were  significantly different from zero at the", round(100 * alpha, 0), "% level(",
+        print(alpha_count_below, "of the  value-error correlations were  significantly different from zero at the",
+              round(100 * alpha, 0), "% level(",
               round(100 * alpha_count_below / count_value_error, 1), "%)")
 
     return spread, spread_rel, time_list, count_value_error

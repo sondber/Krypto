fd49eebbb36fcfce44c144ef47b91c8da093ed35
markusaurelius2 <markuso@stud.ntnu.no>
2018-03-21 17:56:45 +0100

    '

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Sondre import sondre_support_formulas as sup
 from Sondre.sondre_support_formulas import unix_to_timestamp
 
 exc_name="krakeneur"
-start_stamp=unix_to_timestamp(1406851200)
+start_stamp=unix_to_timestamp(1389173189)
 end_stamp=unix_to_timestamp(1514764800)
 
 dis.import_from_csv_w_ticks(exc_name, start_stamp, end_stamp)

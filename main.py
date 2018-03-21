550f09cc1bafc91f3f6b1e9bd2dfea8d1feaaab6
markusaurelius2 <markuso@stud.ntnu.no>
2018-03-21 17:39:01 +0100

    '

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import data_import_support as dis
+from Sondre import sondre_support_formulas as sup
+from Sondre.sondre_support_formulas import unix_to_timestamp
 
 exc_name="krakeneur"
-start_stamp=dis.unix_to_timestamp(1406851200)
-end_stamp=dis.unix_to_timestamp(1514764800)
+start_stamp=unix_to_timestamp(1406851200)
+end_stamp=unix_to_timestamp(1514764800)
 
 dis.import_from_csv_w_ticks(exc_name, start_stamp, end_stamp)

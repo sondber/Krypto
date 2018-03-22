9a0aefe90857f43df7491f7f6ef6f2bea84b80b1
jetonnesen <jetonnesen@users.noreply.github.com>
2018-03-22 15:02:34 +0100

    fixed international

--- international.py
+++ international.py
@@ -1,21 +1,45 @@
 import numpy as np
-import time
-import data_import
 import data_import as di
-import data_import_support
 import plot
-from Jacob import jacob_support as jake_supp
 from Sondre import sondre_support_formulas as supp
-import descriptive_stats as desc
 import data_import_support as dis
-import os
-from matplotlib import pyplot as plt
-import matplotlib.ticker as mtick
-import realized_volatility
 import rolls
-import ILLIQ
 
-exchanges = [0, 4] #select pair
+exchanges = [3, 4]  # select pair
+
+
+remove_extremes = 1
+remove_zero_volume = 1
+remove_zero_bas = 1
+print_cyclical = 1
+print_results = 0
+sum_volumes = 1
+
+if sum_volumes==1:
+    summed_volume = []
+    bitstamp, time_list_bitstampM, prices_bitstampM, volumes_bitstampM = di.get_list(0)
+    coinbase, time_list_coinbaseM, prices_coinbaseM, volumes_coinbaseM = di.get_list(3)
+    kraken, time_list_krakenM, prices_krakenM, volumes_krakenM = di.get_list(5)
+
+    bitstamp_hour_list, prices, volumes_bitstampH = dis.convert_to_hour(time_list_bitstampM, prices_bitstampM, volumes_bitstampM)
+    coinbase_hour_list, prices, volumes_coinbaseH = dis.convert_to_hour(time_list_coinbaseM, prices_coinbaseM, volumes_coinbaseM)
+    kraken_hour_list, prices, volumes_krakenH = dis.convert_to_hour(time_list_krakenM, prices_krakenM, volumes_krakenM)
+
+    j = 0 #coinbase counter
+    t = 0 #kraken counter
+    partsum = 0
+
+    for i in range(len(bitstamp_hour_list)):
+        partsum = volumes_bitstampH[i]
+        if not supp.A_before_B(bitstamp_hour_list[i], kraken_hour_list[t]) and t < len(kraken_hour_list)-1:
+            if bitstamp_hour_list[i] == kraken_hour_list[t]:
+                partsum += volumes_krakenH[t]
+                t += 1
+        if not supp.A_before_B(bitstamp_hour_list[i], coinbase_hour_list[j]) and j < len(coinbase_hour_list)-1:
+            if bitstamp_hour_list[i] == coinbase_hour_list[j]:
+                partsum += volumes_coinbaseH[j]
+                j += 1
+        summed_volume.append(partsum)
 
 exchange_names = []
 
@@ -24,11 +48,13 @@ exc_name1, time_list1M, prices1M, volumes_1M = di.get_list(exc1)
 s_abs, spread_1H, spread_time_1, errs = rolls.rolls(prices1M, time_list1M, kill_output=1)
 exc1_hour_list, prices, volumes_1H = dis.convert_to_hour(time_list1M, prices1M, volumes_1M)
 exchange_names.append(exc_name1)
+
 exc2 = exchanges[1]
 exc_name2, time_list2M, prices2M, volumes_2M = di.get_list(exc2)
 s_abs, spread_2H, spread_time_2, errs = rolls.rolls(prices2M, time_list2M, kill_output=1)
 exc2_hour_list, prices, volumes_2H = dis.convert_to_hour(time_list2M, prices2M, volumes_2M)
 exchange_names.append(exc_name2)
+
 if len(exchanges) > 2:
     exc3 = exchanges[2]
     exc_name3, time_list3M, prices3M, volumes_3M = di.get_list(exc3)
@@ -70,6 +96,69 @@ print(time_list2M[0], time_list2M[-1])
 print(exc_name2, "time hours:", len(spread_2H))
 print(spread_time_2[0], spread_time_2[-1])
 
+hours_to_remove_1 = []
+hours_to_remove_2 = []
+
+if remove_extremes == 1:
+    hours_to_remove_1 = supp.remove_extremes(hours_to_remove_1, volumes_1H, 6000)
+    hours_to_remove_1 = supp.remove_extremes(hours_to_remove_1, spread_1H, 0.01)
+
+    #hours_to_remove_2 = supp.remove_extremes(hours_to_remove_2, volumes_2H, 6000)
+    hours_to_remove_2 = supp.remove_extremes(hours_to_remove_2, spread_2H, 0.01)
+
+    spread_time_1 = np.delete(spread_time_1, hours_to_remove_1)
+    spread_time_2 = np.delete(spread_time_2, hours_to_remove_2)
+
+    volumes_2H = np.delete(volumes_2H, hours_to_remove_2)
+    volumes_1H = np.delete(volumes_1H, hours_to_remove_1)
+
+    spread_1H = np.delete(spread_1H, hours_to_remove_1)
+    spread_2H = np.delete(spread_2H, hours_to_remove_2)
+
+    print("Removed due to extremes:", len(hours_to_remove_1), len(hours_to_remove_2))
+
+if remove_zero_volume == 1:
+    hours_to_remove_1 = []
+    hours_to_remove_2 = []
+    for i in range(len(spread_time_1)):
+        if volumes_1H[i] == 0:
+            hours_to_remove_1.append(i)
+    for j in range(len(spread_time_2)):
+        if volumes_2H[j] == 0:
+            hours_to_remove_2.append(j)
+
+    spread_time_1 = np.delete(spread_time_1, hours_to_remove_1)
+    spread_time_2 = np.delete(spread_time_2, hours_to_remove_2)
+
+    volumes_2H = np.delete(volumes_2H, hours_to_remove_2)
+    volumes_1H = np.delete(volumes_1H, hours_to_remove_1)
+
+    spread_1H = np.delete(spread_1H, hours_to_remove_1)
+    spread_2H = np.delete(spread_2H, hours_to_remove_2)
+
+    print("Removed due to zero volume:", len(hours_to_remove_1), len(hours_to_remove_2))
+
+if remove_zero_bas == 1:
+    hours_to_remove_1 = []
+    hours_to_remove_2 = []
+    for i in range(len(spread_time_1)):
+        if spread_1H[i] == 0:
+            hours_to_remove_1.append(i)
+    for j in range(len(spread_time_2)):
+        if spread_2H[j] == 0:
+            hours_to_remove_2.append(j)
+
+    spread_time_1 = np.delete(spread_time_1, hours_to_remove_1)
+    spread_time_2 = np.delete(spread_time_2, hours_to_remove_2)
+
+    volumes_2H = np.delete(volumes_2H, hours_to_remove_2)
+    volumes_1H = np.delete(volumes_1H, hours_to_remove_1)
+
+    spread_1H = np.delete(spread_1H, hours_to_remove_1)
+    spread_2H = np.delete(spread_2H, hours_to_remove_2)
+
+    print("Removed due to zero bas:", len(hours_to_remove_1), len(hours_to_remove_2))
+
 time_list_master = []
 data_master = []
 
@@ -99,41 +188,98 @@ else:
     longest_name = exc_name2
     shortest_name = exc_name1
 
-print("The longest set is", longest_name, ", while the shortest set is", shortest_name)
+print("The longest set is", longest_name, ", while the shortest set is", shortest_name, "(after removal of extr and zeros))")
 
 prev_j = 0
+j = 0
 print()
 print("Searching......")
 for i in range(0, n):
+    if j == n_shortest - 1:
+        break
     time_stamp = spread_time_longest[i]
-    j = max(0, prev_j)
     if not supp.A_before_B(time_stamp, spread_time_shortest[0]):
-        while spread_time_shortest[j] != time_stamp and j < n_shortest:
-            j += 1
-        spread_row = [spread_longestH[i], spread_shortestH[j], volume_longestH[i], volume_shortestH[j]]
-        spread_row = np.matrix(spread_row)
-        if len(data_master) == 0:
-            data_master = spread_row
-        else:
-            data_master = np.append(data_master, spread_row, axis=0)
-        time_list_master.append(time_stamp)
         prev_j = j
+        while spread_time_shortest[j] != time_stamp and j < n_shortest-1:
+            j += 1
+        if spread_time_shortest[j] == time_stamp:
+            spread_row = [spread_longestH[i], spread_shortestH[j], volume_longestH[i], volume_shortestH[j]]
+            spread_row = np.matrix(spread_row)
+            if len(data_master) == 0:
+                data_master = spread_row
+            else:
+                data_master = np.append(data_master, spread_row, axis=0)
+            time_list_master.append(time_stamp)
+        else:
+            j = prev_j
+
 
 print("Finished searching.")
-
 print("Results:")
 print(time_list_master[0])
 print(time_list_master[-1])
+print("The length of the set is", len(time_list_master))
 
-day_time1, bas_average1, bas_lower1, bas_upper1 = dis.cyclical_average(time_list_master, data_master[:, 0])
-day_time2, bas_average2, bas_lower2, bas_upper2 = dis.cyclical_average(time_list_master, data_master[:, 1])
+if print_cyclical == 1:
+    day_time1, bas_average1, bas_lower1, bas_upper1 = dis.cyclical_average(time_list_master, data_master[:, 0])
+    day_time2, bas_average2, bas_lower2, bas_upper2 = dis.cyclical_average(time_list_master, data_master[:, 1])
 
-day_time1, vol_average1, vol_lower1, vol_upper1 = dis.cyclical_average(time_list_master, data_master[:, 2])
-day_time2, vol_average2, vol_lower2, vol_upper2 = dis.cyclical_average(time_list_master, data_master[:, 3])
+    day_time1, vol_average1, vol_lower1, vol_upper1 = dis.cyclical_average(time_list_master, data_master[:, 2])
+    day_time2, vol_average2, vol_lower2, vol_upper2 = dis.cyclical_average(time_list_master, data_master[:, 3])
 
-name1 = longest_name + "_" + shortest_name + "1"
-name2 = longest_name + "_" + shortest_name + "2"
-plot.intraday(bas_average1, bas_lower1, bas_upper1, name1 + "_bas")
-plot.intraday(bas_average2, bas_lower2, bas_upper2, name2 + "_bas")
-plot.intraday(vol_average1, vol_lower1, vol_upper1, name1 + "_vol")
-plot.intraday(vol_average2, vol_lower2, vol_upper2, name2 + "_vol")
+    name1 = longest_name + "_" + shortest_name + "1"
+    name2 = longest_name + "_" + shortest_name + "2"
+    plot.intraday(bas_average1, bas_lower1, bas_upper1, name1 + "_bas", perc=1)
+    plot.intraday(bas_average2, bas_lower2, bas_upper2, name2 + "_bas", perc=1)
+    plot.intraday(vol_average1, vol_lower1, vol_upper1, name1 + "_vol")
+    plot.intraday(vol_average2, vol_lower2, vol_upper2, name2 + "_vol")
+
+    if sum_volumes == 1:
+        day_time_summed, vol_sum_ave, vol_sum_lower, vol_sum_upper = dis.cyclical_average(bitstamp_hour_list, summed_volume)
+        plot.intraday(vol_sum_ave, vol_sum_lower, vol_sum_upper, "summed_volume_western")
+
+
+#TESTING
+if print_results == 1:
+    for i in range(len(time_list_master)):
+        print(time_list_master[i], data_master[i,:])
+
+
+#print(np.corrcoef(np.transpose(data_master[:,0:2])))
+spread = np.transpose(data_master[:,1])
+volume = np.transpose(data_master[:,2])
+#volume = summed_volume
+print(np.corrcoef(spread,volume))
+
+"""
+test_index = 3
+test_stamp = bitstamp_hour_list[test_index]
+print("Timestamp test", test_stamp)
+test_volume = summed_volume[test_index]
+
+bitstamp_vol = volumes_bitstampH[test_index]
+
+coinbase_vol = 0
+kraken_vol = 0
+
+
+for i in range(len(coinbase_hour_list)):
+    if coinbase_hour_list[i] == test_stamp:
+        coinbase_vol = volumes_coinbaseH[i]
+        print("Timestamp coinbase:", coinbase_hour_list[i])
+        break
+
+for i in range(len(kraken_hour_list)):
+    if kraken_hour_list[i] == test_stamp:
+        kraken_vol = volumes_krakenH[i]
+        print("Timestamp kraken:", kraken_hour_list[i])
+        break
+
+
+tot_vol = bitstamp_vol + coinbase_vol + kraken_vol
+print("Diff:", tot_vol-test_volume)
+print("Volumes:", tot_vol, test_volume)
+
+if tot_vol == test_volume:
+    print("Success")
+"""
